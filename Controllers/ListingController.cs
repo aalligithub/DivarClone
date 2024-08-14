a90642c30722cb82c16abc6aa228167fa22255d7
Ali <83744482+aalligithub@users.noreply.github.com>
2024-08-15 01:40:32 +0330

    Added Listing model, controller and view

---
+++ Controllers/ListingController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace DivarClone.Controllers
+{
+    public class ListingController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ Models/Listing.cs
@@ -0,0 +1,31 @@
+﻿using System.ComponentModel.DataAnnotations;
+using Microsoft.Build.Framework;
+
+namespace DivarClone.Models
+{
+    //enum تعریف کردیم که کتگوری فقط از اعضای از پیش تایین شده انتخاب شود
+    public enum Category
+    {
+        Electronics,
+        Realstate,
+        Vehicles
+    }
+
+    public class Listing
+    {
+        public int Id { get; set; }
+
+        public required string Name { get; set; }
+
+        public required string Description { get; set; }
+
+        public int Price { get; set; }
+
+        public required string Poster { get; set; }
+
+        public Category Category { get; set; }
+
+        public DateTime DateTimeOfPosting { get; set; }
+
+    }
+}
