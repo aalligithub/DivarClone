89e81b062290b1550bcd669237b238390fee8c37
Ali <83744482+aalligithub@users.noreply.github.com>
2024-08-15 21:39:28 +0330

    added listing objects access to the home controller so they pass to view

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -1,4 +1,5 @@
 using System.Diagnostics;
+using DivarClone.Areas.Identity.Data;
 using DivarClone.Models;
 using Microsoft.AspNetCore.Mvc;
 
@@ -7,20 +8,18 @@ namespace DivarClone.Controllers
     public class HomeController : Controller
     {
         private readonly ILogger<HomeController> _logger;
+        private readonly DivarCloneContext _context;
 
-        public HomeController(ILogger<HomeController> logger)
+        public HomeController(ILogger<HomeController> logger , DivarCloneContext context)
         {
             _logger = logger;
+            _context = context;
         }
 
         public IActionResult Index()
         {
-            return View();
-        }
-
-        public IActionResult Privacy()
-        {
-            return View();
+            var listings = _context.Listings.ToList();
+            return View(listings);
         }
 
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
